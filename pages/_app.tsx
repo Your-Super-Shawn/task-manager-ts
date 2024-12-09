fea7bfac87552bd580addac22dd7b04410d0af08
@Your-Super-Shawn <shawnzhanghi@gmail.com>
2024-12-09 18:24:58 +1100

    Revise the home page using MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  weight: "100 900",
+  variable: "--font-geist-sans",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  weight: "100 900",
+  variable: "--font-geist-mono",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${geistSans.variable} ${geistMono.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
